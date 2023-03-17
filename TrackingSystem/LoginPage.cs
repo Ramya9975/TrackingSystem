c4ece246eeb93bf8e0d4a35eab542dcdb2955908
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-03-17 19:37:31 +0530

    Add project files.

---
+++ TrackingSystem/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TrackingSystem
+{
+    public class LoginPage : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "", "" };
+        }
+
+        // GET api/<controller>/5
+     
+        public IHttpActionResult Get(int roleid)
+        {
+            return Redirect(" ");
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
